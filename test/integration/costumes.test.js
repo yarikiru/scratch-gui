a934843ab92767484aea665dd14d62cf5a41d1a9
picklesrus <picklesrus@users.noreply.github.com>
2020-07-07 15:48:48 -0400

    add non-empty test.

--- test/integration/costumes.test.js
+++ test/integration/costumes.test.js
@@ -25,7 +25,9 @@ describe('Working with costumes', () => {
     afterAll(async () => {
         await driver.quit();
     });
-
+    test('empty test', async () => {
+        await expect(1).toEqual(1);
+    }
     // test('Adding a costume through the library', async () => {
     //     // This is needed when running the tests all at once or it just fails...
     //     await driver.quit();
