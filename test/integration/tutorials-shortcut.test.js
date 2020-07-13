5642eece25bee97fe28cb6b329888c942f8ae8af
picklesrus <picklesrus@users.noreply.github.com>
2020-07-13 15:46:31 -0400

    Fix remnants of comments.

--- test/integration/tutorials-shortcut.test.js
+++ test/integration/tutorials-shortcut.test.js
@@ -1,18 +1,18 @@
 import path from 'path';
 import SeleniumHelper from '../helpers/selenium-helper';
-//
+
 const {
     clickText,
     findByXpath,
     getDriver,
     loadUri
 } = new SeleniumHelper();
-//
+
 const uri = path.resolve(__dirname, '../../build/index.html?tutorial=all');
 const uriPrefix = path.resolve(__dirname, '../../build/index.html?tutorial=');
-//
+
 let driver;
-//
+
 describe('Working with shortcut to Tutorials library', () => {
     beforeAll(() => {
         driver = getDriver();
@@ -21,21 +21,16 @@ describe('Working with shortcut to Tutorials library', () => {
     afterAll(async () => {
         await driver.quit();
     });
-    test('empty test', async () => {
-        await expect(1).toEqual(1);
-    });
-
     test('opens with the Tutorial Library showing', async () => {
         await loadUri(uri);
         // make sure there is a tutorial visible that doesn't have a shortcut
         await clickText('Make It Spin');
         await findByXpath('//div[contains(@class, "step-video")]');
     });
-//
     test('can open hidden tutorials', async () => {
         await loadUri(`${uriPrefix}whatsnew`);
         // should open the tutorial video immediately
         await findByXpath('//div[contains(@class, "step-video")]');
     });
-//     // @todo navigating cards, etc.
+    // @todo navigating cards, etc.
 });
